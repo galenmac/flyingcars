abbe49b9a27221deea8f72d7baee6c8a9b636b4b
Galen Macdonald <galenmacdonald@outlook.com>
2018-10-08 18:20:06 -0400

    this is now a totally different project

--- patents.py
+++ patents.py
@@ -5,17 +5,17 @@ from requests_html import HTMLSession
 
 sesh = HTMLSession()
 
-r = sesh.get("https://patents.google.com/?q=(flying+car)&oq=(flying+car)")
+r = sesh.get("https://www.instructables.com/howto/flesh/")
 
-r.html.render()
+# r.html.render()
 
-# items = r.html.find("#resultsContainer")
+items = r.html.find(".explore-cover-item")
 
-items = r.html.find("div .search-results")
-
-print(items)
-# for item in items:
-#         # print(item)
-#         tag = item.find("a", first = True)
-#         url = "https://www.instructables.com"+(tag.attrs.get("href"))
-#         r = session.get(url)
+for item in items:
+        # print(item)
+        tag = item.find("a", first = True)
+        url = "https://www.instructables.com"+(tag.attrs.get("href"))
+        r = sesh.get(url)
+        steps = r.html.find(".step-title")
+        for step in steps:
+            print(step.text)
