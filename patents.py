4c641054cb31243ba8af08cb840a5db155b063d5
Meg Dholakia <meg.dholakia@gmail.com>
2018-10-08 18:20:48 -0400

    Meg's flesh titles call

--- patents.py
+++ patents.py
@@ -5,15 +5,23 @@ from requests_html import HTMLSession
 
 sesh = HTMLSession()
 
-r = sesh.get("https://patents.google.com/?q=(flying+car)&oq=(flying+car)")
+r = sesh.get("https://www.instructables.com/howto/flesh/")
 
-r.html.render()
+#r.html.render()
 
 # items = r.html.find("#resultsContainer")
 
-items = r.html.find("div .search-results")
+items = r.html.find("div .explore-cover-item.cover-item >  .cover-info > .title")
 
 print(items)
+for item in items:
+    print(item.text)
+#items = r.html.find("a")
+print(len(items))
+
+#for item in items:
+#        print(item.find("a"))
+#print(len(items))
 # for item in items:
 #         # print(item)
 #         tag = item.find("a", first = True)
